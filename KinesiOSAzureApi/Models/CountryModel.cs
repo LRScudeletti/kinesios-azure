42c5438fb3147a3490a932f18ddea32b38012bfc
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2022-03-19 11:16:25 -0300

    Added City, Country, Patient, State and SpecialistModel

--- KinesiOSAzureApi/Models/CountryModel.cs
+++ KinesiOSAzureApi/Models/CountryModel.cs
@@ -4,4 +4,6 @@ public class CountryModel
 {
     public int CountryId { get; set; }
     public string CountryName { get; set; }
+    public int UserId { get; set; }
+    public DateTime CountryUpdateDate { get; set; }
 }
---
+++ KinesiOSAzureApi/Models/SpecialistModel.cs
@@ -0,0 +1,15 @@
+﻿namespace KinesiOSAzureApi.Models;
+
+public class SpecialistModel
+{
+    public int SpecialistId { get; set; }
+    public string SpecialistDocument { get; set; }
+    public string SpecialistName { get; set; }
+    public string SpecialistAddress { get; set; }
+    public string SpecialistDistrict { get; set; }
+    public int CityId { get; set; }
+    public int SpecialistPhone { get; set; }
+    public string SpecialistEmail { get; set; }
+    public int UserId { get; set; }
+    public DateTime SpecialistUpdateDate { get; set; }
+}
