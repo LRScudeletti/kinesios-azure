ca07b0244f8a3d3bb95ae5781df813711369171e
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2022-03-24 21:51:10 -0300

    Added State, City and Device entities.

---
+++ KinesiOSAzureApi/Entities/CityEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KinesiOSAzureApi.Entities;
+
+public class CityEntity
+{
+    [Required]
+    public int CityId { get; set; }
+    [Required]
+    public string? CityName { get; set; }
+    [Required]
+    public int StateId { get; set; }
+    [Required]
+    public int UserId { get; set; }
+    [Required]
+    public DateTime CityUpdateDate { get; set; }
+}
---
+++ KinesiOSAzureApi/Entities/StateEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KinesiOSAzureApi.Entities;
+
+public class StateEntity
+{
+    [Required]
+    public int StateId { get; set; }
+    [Required]
+    public string? StateName { get; set; }
+    [Required]
+    public int CountryId { get; set; }
+    [Required]
+    public int UserId { get; set; }
+    [Required]
+    public DateTime StateUpdateDate { get; set; }
+}
