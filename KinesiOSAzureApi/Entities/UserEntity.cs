3c91991fac44ab8b276a4704e84ab04f51f3693a
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2022-03-25 21:19:24 -0300

    Added User entity.

---
+++ KinesiOSAzureApi/Entities/UserEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KinesiOSAzureApi.Entities;
+
+public class UserEntity
+{
+    [Required]
+    public int UserId { get; set; }
+    [Required]
+    public string? Username { get; set; }
+    public int PatientId { get; set; }
+    public int SpecialistId { get; set; }
+    [Required]
+    public string? Password { get; set; }
+    [Required]
+    public int UserIdUpdate { get; set; }
+    [Required]
+    public DateTime UserUpdateDate { get; set; }
+}
