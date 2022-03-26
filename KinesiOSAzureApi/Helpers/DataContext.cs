261fec7b94fcd3ba71636894147f6f2a35b55171
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2022-03-26 11:54:19 -0300

    Added SQL Server DataContext

---
+++ KinesiOSAzureApi/Helpers/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using KinesiOSAzureApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KinesiOSAzureApi.Helpers;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(Configuration.GetConnectionString("KinesiOSAzureSqlServer") ?? throw new InvalidOperationException());
+    }
+
+    public DbSet<User>? Users { get; set; }
+}
