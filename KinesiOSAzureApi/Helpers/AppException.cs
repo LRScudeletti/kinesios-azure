514b445a3b695f4d791ed4297d54439f14a33c6f
Luiz Rogerio Scudeletti <rogerio.scudeletti@gmail.com>
2022-04-07 20:51:39 -0300

    Added AppException class

---
+++ KinesiOSAzureApi/Helpers/AppException.cs
@@ -0,0 +1,15 @@
+using System.Globalization;
+
+namespace KinesiOSAzureApi.Helpers
+{
+    public class AppException : Exception
+    {
+        public AppException()
+        { }
+
+        public AppException(string message) : base(message) { }
+
+        public AppException(string message, params object[] args)
+            : base(string.Format(CultureInfo.CurrentCulture, message, args)) { }
+    }
+}
